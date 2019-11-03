62f8269d78933c150a665838481cda966012fdf9
Youcef <34604972+youcefs21@users.noreply.github.com>
2019-11-03 08:52:41 -0500

    Plz work
    final?

--- FireWatchingDrone/Assets/Code/TreeFireGenerator.cs
+++ FireWatchingDrone/Assets/Code/TreeFireGenerator.cs
@@ -12,9 +12,12 @@ public class TreeFireGenerator : MonoBehaviour
     System.Random rnd;
     public GameObject myPrefab;
     int[,] fireprox;
+    int[,] burnprox;
     public static int x;
     public static int y;
     public static bool[,] onFirebool;
+    float seaLevel = -3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,20 +28,14 @@ public class TreeFireGenerator : MonoBehaviour
         allTrees = new GameObject[x, y];
         rend = new Renderer[x, y];
         fireprox = new int[x, y];
+        burnprox = new int[x, y];
+
         rnd = new System.Random();
         onFirebool = new bool[x,y];
         for (int i = 0; i < x; i++)
         {
 
-            /*
-            if (i <= x / 2)
-            {
-                growBy += 0.1;
-            }
-            else {
-                growBy -= 0.1;
-            }
-            */
+
             growBy = 2*Mathf.Cos((float) i/10);
             
             
@@ -47,7 +44,7 @@ public class TreeFireGenerator : MonoBehaviour
             {
                 //Instantiate(myPrefab, new Vector3(i, 0.5f, j), Quaternion.identity);
                 allTrees[i, j] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                double yess = rnd.NextDouble();
+                double yess = rnd.NextDouble()/3;
                 yess -= 0.5 + growBy;
                 float h = 0;
                 h += (float)yess;
@@ -61,13 +58,28 @@ public class TreeFireGenerator : MonoBehaviour
                 }
                 */
                 h += Mathf.Cos(j / 180) * 2;
-                
+
+
+
+                h += (float)4 * Mathf.Cos((float)j / 30);
+                h += (float)2 * Mathf.Cos((float)j / 12);
+
+
 
 
                 allTrees[i, j].transform.position = new Vector3(i, h, j);
-                
                 rend[i, j] = allTrees[i, j].GetComponent<Renderer>();
+
                 rend[i, j].sharedMaterial = mater[0];
+
+                if (h >= seaLevel)
+                {
+                    rend[i, j].sharedMaterial = mater[0];
+                }else
+                {
+                    rend[i, j].sharedMaterial = mater[3];
+
+                }
 
             }
         }
@@ -132,8 +144,22 @@ public class TreeFireGenerator : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckProxBurn()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (burnprox[i, j] == 5)
+                {
+                    rend[i, j].sharedMaterial = mater[2];
+                }
+            }
+        }
+    }
+
+            // Update is called once per frame
+            void Update()
     {
        
         int count = 0;
@@ -141,49 +167,114 @@ public class TreeFireGenerator : MonoBehaviour
         {
             for (int j = 0; j < y; j++)
             {
-
-                if (Above(i,j) != null && Above(i, j).GetComponent<Renderer>().sharedMaterial == mater[1]) {
-                    count++;
-                }
-                if (Below(i, j) != null && Below(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                if (allTrees[i, j].GetComponent<Renderer>().sharedMaterial == mater[0])
                 {
-                    count++;
+
+
+
+                    if (Above(i, j) != null && Above(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    if (Below(i, j) != null && Below(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    if (Left(i, j) != null && Left(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    if (Right(i, j) != null && Right(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    switch (count)
+                    {
+                        case 0:
+                            fireprox[i, j] = rnd.Next(1000000);
+                            break;
+                        case 1:
+                            fireprox[i, j] = rnd.Next(100);
+                            break;
+                        case 2:
+                            fireprox[i, j] = rnd.Next(50);
+                            break;
+                        case 3:
+                            fireprox[i, j] = rnd.Next(25);
+                            break;
+                        case 4:
+                            fireprox[i, j] = rnd.Next(10);
+
+                            break;
+
+
+                    }
+                    count = 0;
                 }
-                if (Left(i,j) != null && Left(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                else if (allTrees[i, j].GetComponent<Renderer>().sharedMaterial == mater[1])
                 {
-                    count++;
+                    // Fire's Might burn out
+                    if (Above(i, j) != null && Above(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    if (Below(i, j) != null && Below(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    if (Left(i, j) != null && Left(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    if (Right(i, j) != null && Right(i, j).GetComponent<Renderer>().sharedMaterial == mater[1])
+                    {
+                        count++;
+                    }
+                    if (Right(i, j) != null && Right(i, j).GetComponent<Renderer>().sharedMaterial == mater[2])
+                    {
+                        count++;
+                    }
+                    if (Right(i, j) != null && Right(i, j).GetComponent<Renderer>().sharedMaterial == mater[2])
+                    {
+                        count++;
+                    }
+                    if (Right(i, j) != null && Right(i, j).GetComponent<Renderer>().sharedMaterial == mater[2])
+                    {
+                        count++;
+                    }
+                    if (Right(i, j) != null && Right(i, j).GetComponent<Renderer>().sharedMaterial == mater[2])
+                    {
+                        count++;
+                    }
+                    switch (count)
+                    {
+                        case 0:
+                            burnprox[i, j] = rnd.Next(10000);
+                            break;
+                        case 1:
+                            burnprox[i, j] = rnd.Next(1000);
+                            break;
+                        case 2:
+                            burnprox[i, j] = rnd.Next(1000);
+                            break;
+                        case 3:
+                            burnprox[i, j] = rnd.Next(500);
+                            break;
+                        case 4:
+                            burnprox[i, j] = rnd.Next(250);
+
+                            break;
+                    }
+
+                    count = 0;
                 }
-                if (Right(i,j) != null && Right(i,j).GetComponent<Renderer>().sharedMaterial == mater[1])
-                {
-                    count++;
-                }
-                switch (count) {
-                    case 0:
-                        fireprox[i, j] = rnd.Next(1000000);
-                        break;
-                    case 1:
-                        fireprox[i, j] = rnd.Next(100);
-                        break;
-                    case 2:
-                        fireprox[i, j] = rnd.Next(50);
-                        break;
-                    case 3:
-                        fireprox[i, j] = rnd.Next(25);
-                        break;
-                    case 4:
-                        fireprox[i, j] = rnd.Next(10);
 
-                        break;
-
-
-                }
-
-                count = 0;
 
             }
 
         }
         CheckProxFire();
+        CheckProxBurn();
 
 
     }
